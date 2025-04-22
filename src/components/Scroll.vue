1f5ad0c53ea982711f261aad7ed542700bab1894
Arthur Judd <amjudd315@gmail.com>
2025-04-22 15:48:58 -0600

    scroll on hover for tvs

--- src/components/Scroll.vue
+++ src/components/Scroll.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { useTvNavigationStore } from '@/stores/tvNavigation.store';
 import { onBeforeUnmount, onMounted, ref, useSlots } from 'vue';
 
 const scrollArea = ref<HTMLElement | null>(null);
@@ -24,6 +25,14 @@ onMounted(() => {
 			isScrolling.value = false;
 		}, scrollDelay);
 	});
+
+	scrollArea.value?.addEventListener('mousemove', doHoverScroll);
+	scrollArea.value?.addEventListener('mouseleave', () => {
+		if (hoverScrollInterval.value) {
+			clearInterval(hoverScrollInterval.value);
+			hoverScrollInterval.value = null;
+		}
+	});
 })
 
 onBeforeUnmount(() => {
@@ -31,6 +40,69 @@ onBeforeUnmount(() => {
 		clearTimeout(scrollTimeout.value);
 	}
 });
+
+const hoverScrollTime = 25;
+const hoverScrollInterval = ref<number | null>(null);
+const scrollingEdge = ref<string | null>(null);
+
+function doHoverScroll(event: MouseEvent) {
+	if (!useTvNavigationStore().detectedTv) {
+		return;
+	}
+	// First find out if the mouse is the sides of a scroll area
+	const edgeMargin = 20;
+	const scrollArea = event.currentTarget as HTMLElement;
+	const rect = scrollArea.getBoundingClientRect();
+	const mouseX = event.clientX - rect.left;
+	const mouseY = event.clientY - rect.top;
+
+	
+	if (mouseX < edgeMargin) {
+		// Mouse is on the left side
+		scrollingEdge.value = 'left';
+	} else if (mouseX > rect.width - edgeMargin) {
+		// Mouse is on the right side
+		scrollingEdge.value = 'right';
+	} else if (mouseY < edgeMargin) {
+		// Mouse is on the top side
+		scrollingEdge.value = 'top';
+	} else if (mouseY > rect.height - edgeMargin) {
+		// Mouse is on the bottom side
+		scrollingEdge.value = 'bottom';
+	} else {
+		// Mouse is not on the edge
+		scrollingEdge.value = null;
+	}
+
+	function doEdgeScroll(edge) {
+		let distanceLeft = 0;
+		let distanceTop = 0;
+		if (edge === 'left') {
+			distanceLeft = -10;
+		} else if (edge === 'right') {
+			distanceLeft = +10;
+		} else if (edge === 'top') {
+			distanceTop = -10;
+		} else if (edge === 'bottom') {
+			distanceTop = +10;
+		}
+		scrollArea.scrollTo({
+			left: scrollArea.scrollLeft + distanceLeft,
+			top: scrollArea.scrollTop + distanceTop,
+		});
+	}
+
+	if (!scrollingEdge.value && hoverScrollInterval.value) {
+		clearInterval(hoverScrollInterval.value);
+		hoverScrollInterval.value = null;
+		return;
+	}
+	if (scrollingEdge.value && !hoverScrollInterval.value) {
+		doEdgeScroll(scrollingEdge.value);
+		hoverScrollInterval.value = window.setInterval(() => doEdgeScroll(scrollingEdge.value), hoverScrollTime);
+	} 
+	// Then start scroling there in intervals, or stop scrolling
+}
 
 const thumbHover = ref(false);
 
