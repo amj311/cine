71eabc57152df840c4bd486dd4b557d189afdf08
Arthur Judd <amjudd315@gmail.com>
2025-05-01 14:31:13 -0600

    show label while scrolling

--- src/views/browse/PhotoLibraryPage.vue
+++ src/views/browse/PhotoLibraryPage.vue
@@ -66,8 +66,14 @@ function computeTimelineDays() {
 
 
 // TRACK ANCHORS!
+type Anchor = {
+	percent: number;
+	fromTop: number;
+	element: HTMLElement;
+	label: string;
+};
 const trackWrapper = ref<HTMLDivElement>();
-const trackAnchors = ref<Array<{ percent: number, element: HTMLElement, label: string }>>([]);
+const trackAnchors = ref<Array<Anchor>>([]);
 function getAnchors() {
 	if (!trackWrapper.value) {
 		return [];
@@ -80,6 +86,7 @@ function getAnchors() {
 			const percent = ((rect.top - trackWrapper.value!.getBoundingClientRect().top) / trackWrapper.value!.clientHeight) * 100;
 			return {
 				percent,
+				fromTop: rect.top - trackWrapper.value!.getBoundingClientRect().top,
 				element: anchor,
 				label: anchor.getAttribute(anchorAttr) || '',
 			};
@@ -111,6 +118,63 @@ function scrollToAnchor(anchor) {
 	});
 }
 
+let lastScrollTop = 0;
+const scrollDebounceTime = 300;
+let lastScrollEventTime = 0;
+const closestAnchor = ref<Anchor | null>(null);
+const showClosestLabel = ref(false);
+const showClosestLabelTime = 1000;
+const hideClosestLabelTimeout = ref<number | null>(null);
+function doShowClosestLabel() {
+	if (hideClosestLabelTimeout.value) {
+		clearTimeout(hideClosestLabelTimeout.value);
+	}
+	showClosestLabel.value = true;
+	hideClosestLabelTimeout.value = window.setTimeout(() => {
+		showClosestLabel.value = false;
+	}, showClosestLabelTime);
+}
+
+function debounceScrollHandler(event: Event) {
+	if (lastScrollEventTime && Date.now() - lastScrollEventTime < scrollDebounceTime) {
+		return;
+	}
+	lastScrollEventTime = Date.now();
+	const scroller = scrollerRef.value;
+	if (!scroller) {
+		return;
+	}
+	const scrollArea = scroller.scrollArea;
+	if (!scrollArea) {
+		return;
+	}
+	const oldScrollTop = lastScrollTop;
+	const scrollTop = scrollArea.scrollTop;
+
+	if (Math.abs(scrollTop - oldScrollTop) < 50) {
+		console.log("scrolled small")
+		return;
+	}
+
+	lastScrollTop = scrollTop;
+	console.log("scrolled BIG")
+
+	// find the closest anchor
+	const anchors = trackAnchors.value;
+	if (anchors.length === 0) {
+		return;
+	}
+	const allAbove = anchors.filter((anchor) => anchor.fromTop < scrollTop);
+	const closest = allAbove[allAbove.length - 1];
+	closestAnchor.value = closest;
+	doShowClosestLabel();
+}
+
+onMounted(() => {
+	// Get anchors after the DOM is mounted
+	scrollerRef.value?.scrollArea?.addEventListener('scroll', debounceScrollHandler);
+});
+
 </script>
 
 <template>
@@ -121,7 +185,7 @@ function scrollToAnchor(anchor) {
 						<template #default="{ inRange }">
 							<div class="flex flex-column gap-5 mt-3" ref="trackWrapper">
 								<div v-for="day in timelineDays" :key="day.date" class="date-row" :data-track-anchor="day.date">
-									<h2 class="mb-3" style="position: sticky; top: 0; text-shadow: rgba(0, 0, 0, 0.8) 0px 0px 2px">{{ day.date }}</h2>
+									<h2 class="mb-3">{{ day.date }}</h2>
 									<div class="photo-grid">
 										<div v-for="file in day.items" :key="file.relativePath" :id="file.relativePath" class="photo-cell lazy-load" tabindex="0">
 											<div v-if="file.type === 'photo' && inRange[file.relativePath]" style="width: 100%; height: 100%;">
@@ -141,7 +205,13 @@ function scrollToAnchor(anchor) {
 				</Scroll>
 			</div>
 			<div class="track">
-				<div class="track-anchor-item" v-for="(anchor, i) in trackAnchors" @click="() => scrollToAnchor(anchor)" :style="{ top: anchor.percent + '%', height: ((trackAnchors[i+1]?.percent || 100) - anchor.percent) + '%' }" tabindex="0">
+				<div class="track-anchor-item"
+					v-for="(anchor, i) in trackAnchors"
+					:class="{ 'show-label': showClosestLabel && closestAnchor?.label === anchor.label }"
+					@click="() => scrollToAnchor(anchor)"
+					:style="{ top: anchor.percent + '%', height: ((trackAnchors[i+1]?.percent || 100) - anchor.percent) + '%' }"
+					tabindex="0"
+				>
 					<div class="label">{{ anchor.label }}</div>
 					<div class="tick"></div>
 				</div>
@@ -200,8 +270,10 @@ function scrollToAnchor(anchor) {
 				transition: opacity 0.3s;
 				pointer-events: none;
 			}
-			&:hover .label, &:focus .label {
-				opacity: 1;
+			&:hover, &:focus, &.show-label {
+				.label {
+					opacity: 1;
+				}
 			}
 		}
 	}
