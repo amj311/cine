08f960490e764d2bbaee11a22835ca45491e95ee
amj311 <amjudd315@gmail.com>
2025-12-13 21:22:23 -0700

    fix colors and logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -9,7 +9,7 @@ defineProps<{
 		<!-- Draw the popcorn emoji image from above into this svg -->
 		<image id="popcorn" href="/public/public/Popcorn-Emoji-512.png" x="0" y="-13" width="60" height="60" />
 		<path id="curve" fill="transparent" d="M 75,45 C 175,35, 260,35 360,45" />
-		<text fill="white" font-size="70" font-weight="bold">
+		<text fill="white" font-size="68" font-weight="bold">
 			<textPath xlink:href="#curve">
 				OlivePlex
 			</textPath>
