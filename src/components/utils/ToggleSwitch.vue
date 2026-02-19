00431d367db959660e430d6175536c427082b15d
amj311 <amjudd315@gmail.com>
2026-02-19 12:05:13 -0700

    cleanup toggle

---
+++ src/components/utils/ToggleSwitch.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue';
+import ToggleSwitch from 'primevue/toggleswitch';
+
+defineOptions({
+	inheritAttrs: false,
+});
+
+const props = defineProps<{
+	modelValue?: boolean;
+	defaultValue?: boolean;
+}>();
+const attrs = useAttrs();
+
+</script>
+
+<template>
+	<div tabindex="0" role="button" >
+		<ToggleSwitch
+			v-bind="{ ...props, ...attrs }"
+		/>
+	</div>
+</template>
